f17b67007cefcf96e10c9d8145c1d6d10d34fe52
Rober-97 <Rober-97@users.noreply.github.com>
2019-03-13 14:38:33 +0100

    registrazione

--- ODS/src/Control/PasswordControl.java
+++ ODS/src/Control/PasswordControl.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import Model.*;
 
 /*
- * 
+ * implementa la modifica della password, è chiamata da modificaPassword.jsp
  */
 @WebServlet("/PasswordControl")
 public class PasswordControl extends HttpServlet {
